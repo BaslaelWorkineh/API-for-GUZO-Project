c1c75cf2aabb3633e60118bdd68f46c7855aa3b6
Baslael Workineh <baslaworku@gmail.com>
2024-03-05 15:26:23 +0300

    route setup and goal controller

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,14 +1,21 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get Goals'})
-}
-const setGoals = (req, res) => {
+})
+const setGoals = asyncHandler(async (req, res) => {
+    if(!req.body.text)
+    {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message: 'Set Goals'})
-}
-const putGoals = (req, res) => {
+})
+const putGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message:`Update Goal ${req.params.id}`})
-}
-const deleteGoals = (req, res) => {
+})
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message:`Update Goal ${req.params.id}`})
-}
+})
 
 module.exports = {getGoals, setGoals, putGoals, deleteGoals}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const donenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app  = express()
 
+//this middleware is to use the req.body otherwise it will be undefined
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+//.........................
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, ()=>console.log(`server started on ${port}`))
