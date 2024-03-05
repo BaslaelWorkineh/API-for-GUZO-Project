6de34c9d09865339b80c0765296b0fd0387faf6e
Baslael Workineh <baslaworku@gmail.com>
2024-03-05 16:37:03 +0300

    Added the schema

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getGoals, putGoals, setGoals, deleteGoals} = require('../controller/goalController')
+const {getGoals, updatedGoal, setGoals, deleteGoals} = require('../controller/goalController')
 
 router.route('/').get(getGoals).post(setGoals)
-router.route('/:id').delete(deleteGoals).put(putGoals)
+router.route('/:id').delete(deleteGoals).put(updatedGoal)
 
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const donenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const colors = require('colors')
+
+connectDB()
 
 const app  = express()
 
